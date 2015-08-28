7cc044a6a1a5002ea9f344959f374dbf654e2675
Kenji Ohtsuka <kok.fdcm@gmail.com>
2015-08-29 08:32:16 +0900

    modify to print verbose

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 dir=`dirname $0`
-ln -s $dir/.gitignore $HOME/.gitignore
+ln -sv $dir/.gitignore $HOME/.gitignore
 cp -iv $dir/.gitconfig $HOME/
-ln -s $dir/.vimrc     $HOME/.vimrc
+ln -sv $dir/.vimrc     $HOME/.vimrc
