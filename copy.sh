5924f64bfd3de8b4f214ee6fad2f809d98c1e311
Kenji Ohtsuka <kok.fdcm@gmail.com>
2016-03-31 11:16:09 +0900

    feat(copy): copy if there's any difference

--- copy.sh
+++ copy.sh
@@ -1,4 +1,18 @@
+#!/bin/sh
 dir=`dirname $0`
-cp -iv $dir/.gitignore $HOME/
-cp -iv $dir/.gitconfig $HOME/
-cp -iv $dir/.vimrc     $HOME/
+
+isDifferent() {
+  diff "$1" "$2" > /dev/null
+  return $?
+}
+
+copyIfDifferent() {
+  isDifferent "$1" "$2"
+  if [ $? -eq 1 ]; then
+    cp -iv "$1" "$2"
+  fi
+}
+
+copyIfDifferent "$dir/.gitignore" "$HOME/"
+copyIfDifferent "$dir/.gitconfig" "$HOME/"
+copyIfDifferent "$dir/.vimrc"     "$HOME/"
